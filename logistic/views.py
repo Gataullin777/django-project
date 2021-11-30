c3c5668e52ea33f542a87577802b0989d511f939
radif <rudolf18111992@gmail.com>
2021-11-30 14:33:33 +0300

    update stock serializer

--- logistic/views.py
+++ logistic/views.py
@@ -1,17 +1,24 @@
 from rest_framework.viewsets import ModelViewSet
-
+from rest_framework import filters
+from django_filters import rest_framework as rest_filters
 from logistic.models import Product, Stock
 from logistic.serializers import ProductSerializer, StockSerializer
+# from logistic.service import StocksFilter
 
 
 class ProductViewSet(ModelViewSet):
+
     queryset = Product.objects.all()
-    print(queryset)
     serializer_class = ProductSerializer
-    # при необходимости добавьте параметры фильтрации
+    filter_backends = (rest_filters.DjangoFilterBackend, filters.SearchFilter)
+    search_fields = ['title', 'description']
+
+
 
 
 class StockViewSet(ModelViewSet):
+
     queryset = Stock.objects.all()
     serializer_class = StockSerializer
-    # при необходимости добавьте параметры фильтрации
+    filter_backends = (rest_filters.DjangoFilterBackend, filters.SearchFilter)
+    search_fields = ['product']
--- stocks_products/settings.py
+++ stocks_products/settings.py
@@ -46,6 +46,8 @@ INSTALLED_APPS = [
     'rest_framework',
 
     'logistic',
+
+    'django_filters',
 ]
 
 MIDDLEWARE = [
@@ -135,6 +137,8 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-REST_FRAMEWORK = {
-
-}
+# REST_FRAMEWORK = {
+#         'DEFAULT_FILTER_BACKENDS':[
+#             'django_filters.rest_framework.DjangoFilterBackend',
+#         ],
+# }
