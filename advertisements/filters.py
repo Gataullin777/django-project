969dae51edee9c407795a93229380d99dcfe599a
radif <rudolf18111992@gmail.com>
2021-12-15 17:04:07 +0300

    filter implemented

--- advertisements/filters.py
+++ advertisements/filters.py
@@ -1,11 +1,16 @@
 from django_filters import rest_framework as filters
 
-# date__lte
+
 # created_at_before=2021-12-10
 # creator=1
+from advertisements.models import Advertisement
+
 
 class AdvertisementFilter(filters.FilterSet):
     """Фильтры для объявлений."""
     creator = filters.NumberFilter(field_name='creator_id', lookup_expr='exact')
-    created_at_before = filters.DateFromToRangeFilter(field_name='created_at', lookup_expr='lte')
+    created_at = filters.DateFromToRangeFilter()
+    class Meta:
+        model = Advertisement
+        fields = ['creator_id', 'created_at']
 
